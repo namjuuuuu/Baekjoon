d20927766a94bb11e59f7365c684d735bb8308a4
namjuuuuu <knju2002g@gmail.com>
2025-12-30 18:23:25 +0900

    2480 java

---
+++ Java/Bronze/2480/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+    		Scanner sc = new Scanner(System.in);
+        
+        	int A = sc.nextInt();
+        	int B = sc.nextInt();
+        	int C = sc.nextInt();
+        	int total;
+        
+        	if (A == B && B == C) {
+        		total = 10000 + A * 1000;
+        	} else if (A == B || A == C) {
+        		total  = 1000 + A * 100;
+        	} else if (B == C) {
+        		total  = 1000 + B * 100;
+        	} else {
+        		total  = Math.max(A, Math.max(B, C)) * 100;
+        	}
+        	System.out.println(total);
+    	}
+}       
